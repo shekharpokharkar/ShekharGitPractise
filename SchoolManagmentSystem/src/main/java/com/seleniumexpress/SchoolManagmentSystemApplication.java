6939d2088b879de77f76eaa093ebed319ce3957b
shekharpokharkar <sheharpokharkar14@gmail.com>
2025-04-05 22:11:23 +0530

    First Commit

--- SchoolManagmentSystem/src/main/java/com/seleniumexpress/SchoolManagmentSystemApplication.java
+++ SchoolManagmentSystem/src/main/java/com/seleniumexpress/SchoolManagmentSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SchoolManagmentSystemApplication {
 
 	public static void main(String[] args) {
+		System.out.println("***********");
 		SpringApplication.run(SchoolManagmentSystemApplication.class, args);
 	}
 
